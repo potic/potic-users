2328b0ed4cbe51fe644da89f6e37310ec5f6d92b
Yaroslav Yermilov <yermilov17@gmail.com>
2017-10-04 01:45:41 +0300

    #1. implement api for registering new users

--- src/main/groovy/me/potic/users/controller/UserController.groovy
+++ src/main/groovy/me/potic/users/controller/UserController.groovy
@@ -8,6 +8,9 @@ import me.potic.users.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -27,14 +30,14 @@ class UserController {
     @CrossOrigin
     @GetMapping(path = '/user/me')
     @ResponseBody User findUserByAuth0Token(final Principal principal) {
-        log.info 'receive request for /user/me'
+        log.info 'receive GET request for /user/me'
 
         try {
             String socialId = auth0Service.getSocialId(principal.token)
             return userService.findUserBySocialId(socialId)
         } catch (e) {
-            log.error "request for /user/me failed: $e.message", e
-            throw new RuntimeException("request for /user/me failed: $e.message", e)
+            log.error "GET request for /user/me failed: $e.message", e
+            throw new RuntimeException("GET request for /user/me failed: $e.message", e)
         }
     }
 
@@ -42,13 +45,55 @@ class UserController {
     @CrossOrigin
     @GetMapping(path = '/users/ids')
     @ResponseBody List<String> getAllUsersIds() {
-        log.info 'receive request for /users/ids'
+        log.info 'receive GET request for /users/ids'
 
         try {
             return userService.getAllUsersIds()
         } catch (e) {
-            log.error "request for /users/ids failed: $e.message", e
-            throw new RuntimeException("request for /users/ids failed: $e.message", e)
+            log.error "GET request for /users/ids failed: $e.message", e
+            throw new RuntimeException("GET request for /users/ids failed: $e.message", e)
+        }
+    }
+
+    @Timed(name = 'user.POST')
+    @CrossOrigin
+    @PostMapping(path = '/user')
+    @ResponseBody User createNewUser() {
+        log.info 'receive POST request for /user'
+
+        try {
+            return userService.createNewUser()
+        } catch (e) {
+            log.error "POST request for /user failed: $e.message", e
+            throw new RuntimeException("POST request for /user failed: $e.message", e)
+        }
+    }
+
+    @Timed(name = 'user.register.socialId')
+    @CrossOrigin
+    @PostMapping(path = '/user/{userId}/register')
+    @ResponseBody User registerSocialId(@PathVariable('userId') String userId, @RequestParam(value = 'socialId') String socialId) {
+        log.info "receive POST request for /user/$userId/register?socialId=***"
+
+        try {
+            return userService.registerSocialId(userId, socialId)
+        } catch (e) {
+            log.error "receive POST request for /user/$userId/register?socialId=*** failed: $e.message", e
+            throw new RuntimeException("receive POST request for /user/$userId/register?socialId=*** failed: $e.message", e)
+        }
+    }
+
+    @Timed(name = 'user.register.pocketAccessToken')
+    @CrossOrigin
+    @PostMapping(path = '/user/{userId}/register')
+    @ResponseBody User registerPocketAccessToken(@PathVariable('userId') String userId, @RequestParam(value = 'pocketAccessToken') String pocketAccessToken) {
+        log.info "receive POST request for /user/$userId/register?pocketAccessToken=***"
+
+        try {
+            return userService.registerPocketAccessToken(userId, pocketAccessToken)
+        } catch (e) {
+            log.error "receive POST request for /user/$userId/register?pocketAccessToken=*** failed: $e.message", e
+            throw new RuntimeException("receive POST request for /user/$userId/register?pocketAccessToken=*** failed: $e.message", e)
         }
     }
 }
--- src/main/groovy/me/potic/users/service/UserService.groovy
+++ src/main/groovy/me/potic/users/service/UserService.groovy
@@ -48,4 +48,45 @@ class UserService {
             throw new RuntimeException('finding all user ids failed', e)
         }
     }
+
+    User createNewUser() {
+        log.info 'creating new user'
+
+        try {
+            User user = new User()
+            mongoTemplate.save(user)
+            return user
+        } catch (e) {
+            log.error "creating new user failed: $e.message", e
+            throw new RuntimeException('creating new user failed', e)
+        }
+    }
+
+    User registerSocialId(String userId, String socialId) {
+        log.info "registering social id for user $userId"
+
+        try {
+            User user = mongoTemplate.find(query(where('id').is(userId)), User).first()
+            user.socialIds = user.socialIds == null ? [ socialId ] : user.socialIds + socialId
+            mongoTemplate.save(user)
+            return user
+        } catch (e) {
+            log.error "registering social id for user $userId failed: $e.message", e
+            throw new RuntimeException("registering social id for user $userId failed", e)
+        }
+    }
+
+    User registerPocketAccessToken(String userId, String pocketAccessToken) {
+        log.info "registering pocket access token for user $userId"
+
+        try {
+            User user = mongoTemplate.find(query(where('id').is(userId)), User).first()
+            user.pocketAccessToken = pocketAccessToken
+            mongoTemplate.save(user)
+            return user
+        } catch (e) {
+            log.error "registering pocket access token for user $userId failed: $e.message", e
+            throw new RuntimeException("registering pocket access token for user $userId failed", e)
+        }
+    }
 }
